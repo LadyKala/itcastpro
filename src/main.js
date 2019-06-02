387ae3be8cc56e48bc8ad0e968ca7e00cd021a91
kara <3316829105@qq.com>
2019-06-02 20:48:05 +0800

    成功

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from '@/router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  // 注入路由
+  router,
+
   render: h => h(App)
 }).$mount('#app')
